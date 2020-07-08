16732b0f8408a83eb67a8f822640293536c7b581
Aaquib Equbal <44823431+armanaaquib@users.noreply.github.com>
2020-07-08 17:28:51 +0530

    Added: vorpal interface with save command

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const sqlite3 = require('sqlite3').verbose();
+const vorpal = require('vorpal')();
 const JuiceApp = require('./src/juiceApp');
 
 const db = new sqlite3.Database('./db/juices.db');
+const juiceApp = JuiceApp.load(db);
 
-const main = () => {
-  const juiceApp = JuiceApp.load(db);
-  juiceApp
-    .save({ empId: 25275, beverage: 'papaya', qty: 2 })
-    .then(console.table);
-};
+vorpal
+  .command('save <empId> <beverage> <qty>')
+  .description('save a transaction')
+  .alias('s')
+  .autocomplete(['orange', 'papaya'])
+  .action((args, cb) => {
+    const { empId, beverage, qty } = args;
+    juiceApp.save({ empId, beverage, qty }).then((rows) => {
+      console.table(rows);
+      cb();
+    });
+  });
 
-main();
+vorpal.delimiter('juiceApp $ ').show();
